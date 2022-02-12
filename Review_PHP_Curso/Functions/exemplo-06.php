dfbd73d4f3431f2b08e603f442b5c6dfdb6afff8
andre <andredeveloper34@gmail.com>
2022-02-12 18:34:49 -0300

    update

--- Review_PHP_Curso/Functions/exemplo-06.php
+++ Review_PHP_Curso/Functions/exemplo-06.php
@@ -1,41 +1,82 @@
 <?php
-//funções recursivas:
-
 $hierarquia = array(
-    //Inicio CEO
-   array('nome_cargo' => 'CEO',
-         'subordinados'=>array(
-
-             //Inicio: Diretor Comercial
-             array('nome_cargo' => 'Diretor Comercial',
-                    'subordinados'=>array(
-
-                        //Inicio: Gerente de Vendas
-                        array('nome_cargo' => 'Gerente de Vendas')
+    array(
+        'nome_cargo' => 'CEO',
+        'subordinados'=>array(
+            //Inicio: Diretor-Comercial
+            array(
+                'nome_cargo'=>'Diretor Comercial',
+                'subordinados'=>array(
+                    //Inicio: Gerente de Vendas
+                    array(
+                        'nome_cargo'=>'Gerente de Vendas'
                     )
-                ), //Termino: Diretor Comercial
-                //Inicio: Diretor financeiro
-                array(
-                    'nome_cargo' => 'Diretor Financeiro',
-                    'subordinados'=>array(
-                        //Inicio Gerente de Contas a pagar
-                        array(
-                            'nome_cargo' => 'Gerente de Contas a Pagar',
-                            'subordinados'=>array(
-                                
-                            )
-
-                        )
-                        //Termino Gerente de contas a Pagar
-                    )
-                
+                    //Termino: Gerente de Vendas 
                 )
-                //Termino: Diretor Financeiro
+            ),
+            //Termino:Diretor-Comercial
+            //Inicio:Diretor-Financeiro
+            array(
+                'nome_cargo'=>'Diretor Financeiro',
+                'subordinados'=>array(
+                    //Inicio: Gerente de Contas a Pagar
+                    array(
+                        'nome_cargo' => 'Gerente de Contas a Pagar',
+                        'subordinados'=>array(
+                            //Inicio: Supervisor Financeiro de Pagamentos
+                            array(
+                                'nome_cargo' => 'Supervisor Financeiro de Pagamentos'
 
-         )
-    ) //Termino CEO
-);
+                            ) 
+                            //Termino: Supervisor de Pagamentos
+                        )
+
+                    ),
+                    //Termino: Gerente de Contas a Pagar
+                    //Inicio: Gerente de Compras
+                    array(
+                        'nome_cargo' => 'Gerente de Compras',
+                        'subordinados' => array(
+                            //Inicio: Supervisor de Suprimentos
+                            array(
+                                'nome_cargo' => 'Supervisor de Suprimentos'
+
+                            )
+                            //Termino: Supervisor de Suprimentos
+                        )
+
+                    ) 
+                    //Termino: Gerente de Comrpras
+                )
+                
+            )
+            //Termino:Diretor-Financeiro
+        )
+    )//Fim CEO
+);//final do Hierarquia
+
+
+function exibe($cargos)
+{
+    $html = '<ul>';
+
+    foreach($cargos as $cargo):
+        $html .= '<li>';
+
+        echo $html .= $cargo['nome_cargo'];
+        if(isset($cargo['subordinados']) && count($cargo['subordinados']) > 0):
+            $html .= exibe($cargo['subordinados']);
+        endif;
+
+        $html = '</li>';
+
+    endforeach;
+
+
+    $html .= '</ul>';
+
+    return $html;
     
+}
 
-
-?>
+echo exibe($hierarquia);
